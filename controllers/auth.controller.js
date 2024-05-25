6da407943fb77085d4863124f63a0ce554d41ffb
Ulvi Azizli <ulviazizli96@gmail.com>
2024-05-25 18:23:05 +0200

    signup functionalty created

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,9 +3,10 @@ const bcryptjs = require("bcryptjs");
 
 const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
-  const hashedPassword = bcryptjs.hashSync(password, 10);
-  const newUser = new User({ username, email, password: hashedPassword });
+
   try {
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    const newUser = new User({ username, email, password: hashedPassword });
     await newUser.save();
     res.status(201).json("user created successfully");
   } catch (error) {
