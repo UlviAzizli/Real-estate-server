064672e3a67eb88a58b055ca9f98306aa84609f3
Ulvi Azizli <ulviazizli96@gmail.com>
2024-05-27 11:33:17 +0200

    sign in functionalty finished

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,7 +1,8 @@
-const { signup } = require("../controllers/auth.controller.js");
+const { signup, signin } = require("../controllers/auth.controller.js");
 
 const router = require("express").Router();
 
 router.post("/signup", signup);
+router.post("/signin", signin);
 
 module.exports = router;
