81de742abb2ef04e3106f22acf017c8e8b118221
Ulvi Azizli <ulviazizli96@gmail.com>
2024-05-28 14:08:47 +0200

    google auth done

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,8 +1,9 @@
-const { signup, signin } = require("../controllers/auth.controller.js");
+const { google, signup, signin } = require("../controllers/auth.controller.js");
 
 const router = require("express").Router();
 
 router.post("/signup", signup);
 router.post("/signin", signin);
+router.post("/google", google);
 
 module.exports = router;
