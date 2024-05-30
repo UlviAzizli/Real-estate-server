2413fa004a9cdde481673d2255846359070d13b8
Ulvi Azizli <ulviazizli96@gmail.com>
2024-05-30 12:09:00 +0200

    correct error mistake

--- controllers/listing.controller.js
+++ controllers/listing.controller.js
@@ -5,7 +5,7 @@ const createListing = async (req, res, next) => {
     const listing = await Listing.create(req.body);
     return res.status(201).json(listing);
   } catch (error) {
-    error(next);
+    next(error);
   }
 };
 
