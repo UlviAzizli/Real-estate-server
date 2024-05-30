183466f5555811088d68cfc41a50c50ed9423eb3
Ulvi Azizli <ulviazizli96@gmail.com>
2024-05-30 15:35:42 +0200

    create update listing route

--- routes/listing.route.js
+++ routes/listing.route.js
@@ -2,11 +2,13 @@ const express = require("express");
 const {
   createListing,
   deleteListing,
+  updateListing,
 } = require("../controllers/listing.controller.js");
 const { verifyToken } = require("../utils/verifyUser.js");
 const router = express.Router();
 
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
+router.post("/update/:id", verifyToken, updateListing);
 
 module.exports = router;
