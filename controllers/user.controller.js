fe1057dde91d4d820e8bfa46c3d077ad8c06ed6b
Ulvi Azizli <ulviazizli96@gmail.com>
2024-05-30 12:47:04 +0200

    create get user listing route

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,21 @@
+const Listing = require("../models/Listing.model.js");
+const { errorHandler } = require("../utils/error.js");
+
 const test = (req, res, next) => {
   res.json({ message: "All good in here s" });
 };
 
-module.exports = { test };
+const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, "You can only view your own listings!"));
+  }
+};
+
+module.exports = { test, getUserListings };
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,7 +1,9 @@
-const { test } = require("../controllers/user.controller.js");
+const { test, getUserListings } = require("../controllers/user.controller.js");
+const { verifyToken } = require("../utils/verifyUser.js");
 
 const router = require("express").Router();
 
 router.get("/test", test);
+router.get("/listing/:id", verifyToken, getUserListings);
 
 module.exports = router;
