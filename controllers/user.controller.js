cff21bbdec0b641da714fd080f343d267f3faef3
Ulvi Azizli <ulviazizli96@gmail.com>
2024-06-12 18:05:21 +0200

    create update api route

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,8 +1,39 @@
 const Listing = require("../models/Listing.model.js");
+const User = require("../models/User.model.js");
 const { errorHandler } = require("../utils/error.js");
+const bcryptjs = require("bcryptjs");
 
 const test = (req, res, next) => {
   res.json({ message: "All good in here s" });
+};
+
+const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "You can only update your own account"));
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
+
+    const { password, ...rest } = updatedUser._doc;
+
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
 
 const getUserListings = async (req, res, next) => {
@@ -18,4 +49,4 @@ const getUserListings = async (req, res, next) => {
   }
 };
 
-module.exports = { test, getUserListings };
+module.exports = { test, getUserListings, updateUser };
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,9 +1,14 @@
-const { test, getUserListings } = require("../controllers/user.controller.js");
+const {
+  test,
+  getUserListings,
+  updateUser,
+} = require("../controllers/user.controller.js");
 const { verifyToken } = require("../utils/verifyUser.js");
 
 const router = require("express").Router();
 
 router.get("/test", test);
+router.post("/update/:id", verifyToken, updateUser);
 router.get("/listings/:id", verifyToken, getUserListings);
 
 module.exports = router;
